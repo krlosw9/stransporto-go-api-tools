bacf7f2458b535476e37b4c52961c44e0efc2f63
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-05-08 12:32:02 -0500

    Se coloca publica estructura Response

--- util/response.go
+++ util/response.go
@@ -5,14 +5,14 @@ const (
 	Message = "message"
 )
 
-type response struct {
+type Response struct {
 	MessageType string      `json:"message_type"`
 	Message     string      `json:"message"`
 	Data        interface{} `json:"data"`
 }
 
-func NewResponse(messageType string, message string, data interface{}) response {
-	return response{
+func NewResponse(messageType string, message string, data interface{}) Response {
+	return Response{
 		messageType,
 		message,
 		data,
